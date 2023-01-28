5f5f4259a0588e84483166a706b3d937298f95f8
Cesar <cesar.m.rodriguez@tec.mx>
2023-01-27 19:03:46 -0600

    Today class 27-01-2023

---
+++ 10) CombineTest/CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by César Manuel on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
